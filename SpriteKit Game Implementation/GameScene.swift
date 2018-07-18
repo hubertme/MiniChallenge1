ce80e53ff328e4258e783b51c1f70b778025ebce
Hubert Tatra <hubert.wangg@gmail.com>
2018-07-18 11:31:53 +0700

    Generate Obstacles ✅

--- SpriteKit Game Implementation/GameScene.swift
+++ SpriteKit Game Implementation/GameScene.swift
@@ -12,31 +12,39 @@ import GameplayKit
 class GameScene: SKScene {
     
     var kite = SKSpriteNode()
-    var obstacle = SKSpriteNode()
+//    var obstacle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         // Declaration as self.childNode
         kite = self.childNode(withName: "kite") as! SKSpriteNode
-        obstacle = self.childNode(withName: "obstacle") as! SKSpriteNode
+//        obstacle = self.childNode(withName: "obstacle") as! SKSpriteNode
         
         kite.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 0))
-        obstacle.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 0))
+//        obstacle.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 0))
         
 //        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
 //        border.friction = 0
 //        border.restitution = 0.8
 //        self.physicsBody = border
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
-        print("Hi")
+        generateObstacle()
     }
     
     func generateObstacle(){
-//        let obstacles = self.childNode(withName: "obstacles") as! SKSpriteNode
-        var obstacles = SKSpriteNode()
-        let posX = arc4random_uniform(320)-160
-//        obstacle.physicsBody?.
-//        obstacles = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 100), center: CGPoint(x: CGFloat(posX), y: 420))
+        print("ok")
+        let posX = Int(arc4random_uniform(320))-180
+        print(posX)
+        
+        let obstacles = SKShapeNode(rect: CGRect(x: posX, y: 220, width: 60, height: 150))
+        obstacles.physicsBody? = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 100))
+        obstacles.physicsBody?.isDynamic = true
+        obstacles.physicsBody?.affectedByGravity = true
+        obstacles.physicsBody?.usesPreciseCollisionDetection = true
+        
+        obstacles.fillColor=#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        addChild(obstacles)
     }
 }
