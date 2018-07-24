1fffff2bb5a6b52a54b764218b53e9a4649e22b1
Hubert Wang <hubert.tatra@gmail.com>
2018-07-24 09:03:44 +0700

    Anak Layangan Ver. 1.0

--- SpriteKit Game Implementation/GameScene.swift
+++ SpriteKit Game Implementation/GameScene.swift
@@ -22,7 +22,7 @@ extension Array {
 let kiteTexture = SKTexture(imageNamed: "kite-1")
 var kite = SKSpriteNode(texture: kiteTexture)
 
-var second = 0
+var second = 0.0
 var isOver = true
 
 struct PhysicsCategory {
@@ -44,9 +44,11 @@ class GameScene: SKScene {
     
     var progressBar = SKSpriteNode()
     
+    var timeObs = 0.1
+    
     override func didMove(to view: SKView) {
         
-        self.scene?.backgroundColor = #colorLiteral(red: 0.6161276698, green: 0.9302651286, blue: 1, alpha: 1)
+        self.scene?.backgroundColor = #colorLiteral(red: 0.4871386886, green: 0.5004682541, blue: 0.497177124, alpha: 1)
       
         physicsWorld.contactDelegate = self
         
@@ -56,7 +58,8 @@ class GameScene: SKScene {
         addChild(progressBar)
         let moveKite = SKAction.moveTo(y: -175, duration: 4)
         
-        let action = SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 1), SKAction.run(generateObstacle), SKAction.run(updateScore), SKAction.run(checkWin),  SKAction.run(changeBG)]))
+        let generate  = SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: timeObs), SKAction.run(generateObstacleV2)]))
+        let action = SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 1), SKAction.run(updateScore), SKAction.run(checkWin)]))
         
         let actionCloudRepeat = SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 7), SKAction.run(generateClouds)]))
         let actionCloud = SKAction.sequence([SKAction.wait(forDuration: 2),SKAction.run(generateClouds)])
@@ -64,6 +67,7 @@ class GameScene: SKScene {
         run(actionCloud){
             self.run(actionCloudRepeat)
             self.run(action)
+            self.run(generate)
         }
         
         kite.scale(to: CGSize(width: 500, height: 500))
@@ -111,16 +115,16 @@ class GameScene: SKScene {
       }
   
     func changeBG() {
-        let orange = #colorLiteral(red: 1, green: 0.7476941943, blue: 0.2104941905, alpha: 1)
+        let orange = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
 
-        let bgChange = SKAction.colorize(with: orange, colorBlendFactor: 1.0, duration: 30)
+        let bgChange = SKAction.colorize(with: orange, colorBlendFactor: 1.0, duration: 2)
         run(bgChange)
       }
   
   //  Version 01 > More realistic & natural but still not moving
     func generateClouds() {
         let posX = Int(arc4random_uniform(320))-180
-        let size = CGSize(width: 300, height: 300)
+        var size = CGSize(width: 300, height: 300)
         let actionMove = SKAction.move(to: CGPoint(x: posX, y: -1000), duration: TimeInterval(20))
         let actionMoveDone = SKAction.removeFromParent()
         let cloudsString = arrayClouds.randomElement()
@@ -129,6 +133,38 @@ class GameScene: SKScene {
         clouds.scale(to: size)
         addChild(clouds)
         clouds.run(SKAction.sequence([actionMove,actionMoveDone]))
+        
+        if (second>=19){
+            size = CGSize(width: 0, height: 0)
+        }
+    }
+    
+    func generateObstacleV2() {
+        let posX = Int(arc4random_uniform(320))-180
+        let randHeight = CGFloat(arc4random_uniform(15) + 20)
+        let size = CGSize(width: 4, height: randHeight)
+        
+        let rain = SKSpriteNode(color: #colorLiteral(red: 0, green: 0.6758572459, blue: 0.9273018241, alpha: 1), size: size)
+        rain.position = CGPoint(x: posX, y: 420)
+        rain.physicsBody = SKPhysicsBody(rectangleOf: size)
+        
+        //        rain.zRotation = 10.toRadian
+        
+        
+        rain.physicsBody?.isDynamic = true
+        rain.physicsBody?.collisionBitMask = PhysicsCategory.kite
+        rain.physicsBody?.categoryBitMask = PhysicsCategory.obstacles
+        rain.physicsBody?.contactTestBitMask = PhysicsCategory.kite
+        rain.physicsBody?.restitution = 1
+        //        obstacles.physicsBody?.allowsRotation = true
+        
+        addChild(rain)
+        
+        //        let time = Int(arc4random_uniform(4))+4
+        let actionMove = SKAction.move(to: CGPoint(x: posX, y: -1000), duration: TimeInterval(2))
+        actionMove.timingMode = .easeIn
+        let actionMoveDone = SKAction.removeFromParent()
+        rain.run(SKAction.sequence([actionMove,actionMoveDone]))
     }
     
     func generateObstacle(){
@@ -168,25 +204,25 @@ class GameScene: SKScene {
             let time = Int(arc4random_uniform(4))+1
             let actionMove = SKAction.move(to: CGPoint(x: posX, y: -1000), duration: TimeInterval(time))
             let actionMoveDone = SKAction.removeFromParent()
-            obstacles.run(SKAction.sequence([actionMove,actionMoveDone]))
+            obstacles.run(SKAction.sequence([actionMove,actionMoveDone, SKAction.run(changeBG)]))
         }
         
     }
     
     func updateScore(){
-        second+=1
+        second+=0.5
         progressBar.removeFromParent()
         if (second<0){
             second = 0
         }
         print(second)
-        progressBar = SKSpriteNode(color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), size: CGSize(width: Double(second)/10.0 * 375, height: 30))
+        progressBar = SKSpriteNode(color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), size: CGSize(width: Double(second)/3.0 * 375, height: 30))
         progressBar.position = CGPoint(x: -187.5, y: 350)
         addChild(progressBar)
     }
     
     func checkWin(){
-        if (second==20){
+        if (second>=6){
             removeAllActions()
             gameOver()
         }
@@ -203,12 +239,43 @@ extension GameScene: SKPhysicsContactDelegate{
     func didBegin(_ contact: SKPhysicsContact) {
         if (contact.collisionImpulse >= 0.4) && (contact.bodyA.categoryBitMask == PhysicsCategory.kite) && (contact.bodyB.categoryBitMask == PhysicsCategory.obstacles) {
             print("Hit!")
-            second-=2
+            
+            if (second>=3){
+                second-=0.375
+            }
+            else{
+                second-=0.25
+            }
+            
+//            run(SKAction.playSoundFileNamed("waterDrop.wav", waitForCompletion: false))
+//            let notification = UINotificationFeedbackGenerator()
+//            notification.notificationOccurred(.warning)
+//            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            
+//            Version 2
+            let splashPath = Bundle.main.path(forResource: "Splash", ofType: "sks")!
+            let splash = NSKeyedUnarchiver.unarchiveObject(withFile: splashPath) as! SKEmitterNode
+            
+            splash.particleColor = #colorLiteral(red: 0, green: 0.6758572459, blue: 0.9273018241, alpha: 1)
+            
+            let newX = contact.contactPoint.x
+            let newY = contact.contactPoint.y - 20
+            
+            splash.position = CGPoint(x: newX, y: newY)
+            splash.targetNode = self
+            
+            contact.bodyB.node?.run(SKAction.removeFromParent())
+            
+            //            kite.position.y -= CGFloat(1)
+            print(kite.position.y)
+            
+            addChild(splash)
             run(SKAction.playSoundFileNamed("waterDrop.wav", waitForCompletion: false))
             let notification = UINotificationFeedbackGenerator()
             notification.notificationOccurred(.warning)
-//            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
         }
+        
+        
     }
     
     func gameOver() {
@@ -218,6 +285,10 @@ extension GameScene: SKPhysicsContactDelegate{
             run(SKAction.playSoundFileNamed("winningSound.wav", waitForCompletion: false))
             let sun = SKSpriteNode(imageNamed: "sun")
             addChild(sun)
+//            SKAction.run(changeBG)
+            
+            let bgChange = SKAction.colorize(with: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), colorBlendFactor: 1.0, duration: 3)
+            run(bgChange)
             
             sun.position = CGPoint(x: 0, y: 600)
             sun.size = CGSize(width: 300, height: 300)
@@ -227,9 +298,10 @@ extension GameScene: SKPhysicsContactDelegate{
             
         }
         else{
-            let flewAway = SKAction.move(to: CGPoint(x: 0, y: 600000), duration: 5000)
+            let flewAway = SKAction.move(to: CGPoint(x: 0, y: -600000), duration: 5000)
             kite.run(flewAway)
-            removeAllActions()
+//            removeAllActions()
+//            SKAction.run(generateObstacleV2)
             tail.removeFromParent()
             let loseSoundEffect = SKAction.playSoundFileNamed("losingSound.wav", waitForCompletion: false)
             run(loseSoundEffect)
